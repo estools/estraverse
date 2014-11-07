9b22404c021fc1b748c3027da8f54e7afc88fccb
Yusuke Suzuki <utatane.tea@gmail.com>
2014-11-08 01:45:31 +0900

    version 1.7.2-dev starts

--- estraverse.js
+++ estraverse.js
@@ -819,7 +819,7 @@
         return tree;
     }
 
-    exports.version = '1.5.1-dev';
+    exports.version = '1.7.2-dev';
     exports.Syntax = Syntax;
     exports.traverse = traverse;
     exports.replace = replace;
