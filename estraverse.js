901541eaf636627c6ec5b7f8ddf5d56c0644d8c0
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-02-08 22:24:22 +0900

    Fix version following `package.json`
    This version should be the same as the version defined in
    `package.json`.

--- estraverse.js
+++ estraverse.js
@@ -828,7 +828,7 @@
         return tree;
     }
 
-    exports.version = '1.8.1-dev';
+    exports.version = '1.9.2';
     exports.Syntax = Syntax;
     exports.traverse = traverse;
     exports.replace = replace;
