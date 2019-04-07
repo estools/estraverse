022f9ecb70b7a680fc5721ee4252b05fd0aa83ff
Peter Müller <munter@fumle.dk>
2019-04-07 11:28:44 +0200

    Add support for dynamic Import expressions. See https://tc39.github.io/proposal-dynamic-import/

--- estraverse.js
+++ estraverse.js
@@ -108,6 +108,7 @@
         GeneratorExpression: 'GeneratorExpression',  // CAUTION: It's deferred to ES7.
         Identifier: 'Identifier',
         IfStatement: 'IfStatement',
+        Import: 'ImportExpression',
         ImportDeclaration: 'ImportDeclaration',
         ImportDefaultSpecifier: 'ImportDefaultSpecifier',
         ImportNamespaceSpecifier: 'ImportNamespaceSpecifier',
@@ -182,6 +183,7 @@
         GeneratorExpression: ['blocks', 'filter', 'body'],  // CAUTION: It's deferred to ES7.
         Identifier: [],
         IfStatement: ['test', 'consequent', 'alternate'],
+        Import: ['argument'],
         ImportDeclaration: ['specifiers', 'source'],
         ImportDefaultSpecifier: ['local'],
         ImportNamespaceSpecifier: ['local'],
